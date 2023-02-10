c7661fee9b8e5e537440790314688d102896d523
Kamran <311.kamran@gmail.com>
2023-02-11 02:01:11 +0400

    Delete -> Admin Complete

--- ECommerceWithMVVM/Domain/ViewModels/AdminViewModel.cs
+++ ECommerceWithMVVM/Domain/ViewModels/AdminViewModel.cs
@@ -1,15 +1,11 @@
-﻿using ECommerceWithMVVM.Commands;
-using ECommerceWithMVVM.DataAccess;
-using ECommerceWithMVVM.DataAccess.Repositories;
-using ECommerceWithMVVM.Domain.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using ECommerceWithMVVM.Commands;
+using ECommerceWithMVVM.DataAccess;
+using System.Collections.ObjectModel;
+using ECommerceWithMVVM.Domain.Abstractions;
+using ECommerceWithMVVM.DataAccess.Repositories;
 
 namespace ECommerceWithMVVM.Domain.ViewModels
 {
@@ -42,18 +38,42 @@ namespace ECommerceWithMVVM.Domain.ViewModels
 
             DeleteCommand = new RelayCommand((o) =>
             {
-                //  var dataGrid = o as DataGrid;
-                //
-                //  if (_isCustomer is true)
-                //  {
-                //      var customer = dataGrid.SelectedItem as Customer;
-                //      _customerRepo.DeleteData(customer);
-                //  }
-                //  else
-                //  {
-                //      var product = dataGrid.SelectedItem as Product;
-                //      _productRepo.DeleteData(product);
-                //  }
+
+                var myGrid = o as Grid;
+
+                var stackpanel = myGrid.Children[1] as StackPanel;
+                var customer_radio_button = stackpanel.Children[0] as RadioButton;
+                var product_radio_button = stackpanel.Children[1] as RadioButton;
+                var scroll = myGrid.Children[2] as ScrollViewer;
+                var dataGrid = scroll.Content as DataGrid;
+
+
+                if (customer_radio_button.IsChecked is true)
+                {
+                    try
+                    {
+                        var customer = dataGrid.SelectedItem as Customer;
+                        _customerRepo.DeleteData(customer);
+                        MessageBox.Show("Successfully, Deleted Customer", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"{ex.Message}", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                }
+                else if (product_radio_button.IsChecked is true)
+                {
+                    try
+                    {
+                        var product = dataGrid.SelectedItem as Product;
+                        _productRepo.DeleteData(product);
+                        MessageBox.Show("Successfully, Deleted Product", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"{ex.Message}", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                }
 
 
             });
@@ -66,24 +86,24 @@ namespace ECommerceWithMVVM.Domain.ViewModels
                 var customer_radio_button = stackpanel.Children[0] as RadioButton;
                 var product_radio_button = stackpanel.Children[1] as RadioButton;
                 var scroll = myGrid.Children[2] as ScrollViewer;
-                var dataGrid =scroll.Content as DataGrid;
+                var dataGrid = scroll.Content as DataGrid;
 
 
-                 if (dataGrid.ItemsSource != null)
-                 {
-                     dataGrid.ItemsSource = null;
-                 }
-                 
-                 
-                 if (customer_radio_button.IsChecked is true)
-                 {
-                     dataGrid.ItemsSource = _customerRepo.GetAllData();
-                 }
-                 else if(product_radio_button.IsChecked is true)
-                 {
-                     dataGrid.ItemsSource = _productRepo.GetAllData();
-                 }
-                 else
+                if (dataGrid.ItemsSource != null)
+                {
+                    dataGrid.ItemsSource = null;
+                }
+
+
+                if (customer_radio_button.IsChecked is true)
+                {
+                    dataGrid.ItemsSource = _customerRepo.GetAllData();
+                }
+                else if (product_radio_button.IsChecked is true)
+                {
+                    dataGrid.ItemsSource = _productRepo.GetAllData();
+                }
+                else
                     MessageBox.Show("Zehmet olmasa secimlerden birini edin", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
 
             });
